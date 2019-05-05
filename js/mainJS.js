a2037e2a8a27bc993b1bc9d230a05eb8c53e2e2a
Yaroslavavav <yaroslavavav96@gmail.com>
2019-05-05 21:04:02 +0300

    model done

---
+++ js/mainJS.js
@@ -0,0 +1,11 @@
+$(document).ready(function () { 
+ $('.menu').on('click', function(e) {
+    e.preventDefault()
+  $('.navbar-burger').toggleClass('burger-open');
+ });
+
+ 	$('.slider').slick({
+   arrows: false,
+   autoplay: true
+   });
+});
